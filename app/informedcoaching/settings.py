c74fb983665e214368fb59965ad55eaeab77e480
Matthew Zomorodi <matthewazomorodi@gmail.com>
2024-06-11 16:35:04 -0400

    update environment config

--- app/informedcoaching/settings.py
+++ app/informedcoaching/settings.py
@@ -12,9 +12,13 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
-import environ
-env = environ.Env()
-environ.Env.read_env()
+# import environ
+# env = environ.Env()
+# environ.Env.read_env()
+
+print(__name__)
+
+from . import environment as ENV
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,12 +29,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.str('SECRET_KEY')
+SECRET_KEY = ENV.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DEBUG', default=False)
+DEBUG = ENV.DEBUG
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+ALLOWED_HOSTS = ENV.ALLOWED_HOSTS
 
 
 # Application definition
@@ -80,10 +84,10 @@ WSGI_APPLICATION = 'informedcoaching.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': ENV.DATABASES.get('default', {
+        'ENGINE' : 'django.db.backends.sqlite3',
+        'NAME' : BASE_DIR / 'db.sqlite3'
+    })
 }
 
 
